b7d80544a0494db19cc26ec882b067c1eb353a8b
duywin102 <duywin102@users.noreply.github.com>
2021-03-19 10:08:26 +0700

    Phan1_WebsitegoiWebServiceAndRestfulAPI
    Phan1_WebsitegoiWebServiceAndRestfulAPI

---
+++ NguyenDuyThang_5951071100/NguyenDuyThang_5951071100/Models/db.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using NguyenDuyThang_5951071100.Models;
+
+namespace NguyenDuyThang_5951071100.Models
+{
+    public class db
+    {// "Provider=SQLNCLI11;" + 
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-4A00V1R\SQLEXPRESS;Initial Catalog=DemoCRUD;Integrated Security=True");
+      
+        // Select
+
+        public DataSet Empget(Employee emp, out string msg)
+        {
+            DataSet ds = new DataSet();
+            msg = "";
+
+            try
+            {
+                SqlCommand com = new SqlCommand("Sp_Employee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Sr_no", emp.Sr_no);
+                com.Parameters.AddWithValue("@Emp_name", emp.Emp_name);
+                com.Parameters.AddWithValue("@City", emp.City);
+                com.Parameters.AddWithValue("@STATE", emp.State);
+                com.Parameters.AddWithValue("@Country", emp.Country);
+                com.Parameters.AddWithValue("@Department", emp.Department);
+                com.Parameters.AddWithValue("@flag", emp.Flag);
+
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                da.Fill(ds);
+                msg = "OK";
+
+                return ds;
+            }
+            catch(Exception ex)
+            {
+                msg = ex.Message;
+                return ds;
+            }
+        }
+
+        // insert and update
+
+        public string Empdml(Employee emp, out string msg) {
+            msg = "";
+            try
+            {
+                SqlCommand com = new SqlCommand("Sp_Employee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@Sr_no", emp.Sr_no);
+                com.Parameters.AddWithValue("@Emp_name", emp.Emp_name);
+                com.Parameters.AddWithValue("@City", emp.City);
+                com.Parameters.AddWithValue("@STATE", emp.State);
+                com.Parameters.AddWithValue("@Country", emp.Country);
+                com.Parameters.AddWithValue("@Department", emp.Department);
+                com.Parameters.AddWithValue("@flag", emp.Flag);
+
+                con.Open();
+                com.ExecuteNonQuery();
+                con.Close();
+                msg = "OK";
+                return msg;
+            }
+            catch (Exception ex) {
+                if (con.State == ConnectionState.Open) {
+                    con.Close();
+                }
+
+                msg = ex.Message;
+                return msg;
+            }
+        }
+    }
+}
